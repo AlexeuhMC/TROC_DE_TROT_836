a2df8ee7129c4bd3a6f6f6d92d8c296864b544ad
Alexandra Mercado <xcmercadoxc@gmail.com>
2022-04-01 12:36:03 +0200

    a-check-finitions

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,12 +7,12 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 #   Character.create(name: 'Luke', movie: movies.first)
 
-e_scooter1 = EScooter.create( name: 'DUCATI' ,  description: 'Très bon état, neuve' ,  price: 7 ,  street_name: 'Rue droite' ,  disponibility_start: 30/03/2022,  disponibility_end: 30/04/2022)
-e_scooter2 = EScooter.create( name: 'Eclair de la nuit' ,  description: 'Rapide comme un éclair' ,  price: 15 ,  street_name: 'Boulevard de Cimiez' ,  disponibility_start: 30/03/2022 ,  disponibility_end: 30/04/2022 )
-e_scooter3 = EScooter.create( name: 'MoovWay' ,  description: 'Jusqu a 25km/heure, possède 3 modes, bonne tenue de route' ,  price: 5 ,  street_name: 'Avenue Pierre Isnard' ,  disponibility_start: 30/03/2022 ,  disponibility_end: 30/04/2022 )
-e_scooter4 = EScooter.create( name: 'Star Wars' ,  description: 'Que la force soit avec vous' ,  price: 2 ,  street_name: 'Avenue Thiers' ,  disponibility_start: 30/03/2022 , disponibility_end: 30/04/2022 )
+# e_scooter1 = EScooter.create( name: 'DUCATI' ,  description: 'Très bon état, neuve' ,  price: 7 ,  street_name: 'Rue droite' ,  disponibility_start: 30/03/2022,  disponibility_end: 30/04/2022)
+# e_scooter2 = EScooter.create( name: 'Eclair de la nuit' ,  description: 'Rapide comme un éclair' ,  price: 15 ,  street_name: 'Boulevard de Cimiez' ,  disponibility_start: 30/03/2022 ,  disponibility_end: 30/04/2022 )
+# e_scooter3 = EScooter.create( name: 'MoovWay' ,  description: 'Jusqu a 25km/heure, possède 3 modes, bonne tenue de route' ,  price: 5 ,  street_name: 'Avenue Pierre Isnard' ,  disponibility_start: 30/03/2022 ,  disponibility_end: 30/04/2022 )
+# e_scooter4 = EScooter.create( name: 'Star Wars' ,  description: 'Que la force soit avec vous' ,  price: 2 ,  street_name: 'Avenue Thiers' ,  disponibility_start: 30/03/2022 , disponibility_end: 30/04/2022 )
 
-p e_scooter1
-p e_scooter2
-p e_scooter3
-p e_scooter4
+# p e_scooter1
+# p e_scooter2
+# p e_scooter3
+# p e_scooter4
